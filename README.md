61cb4ecde6e8bc82569eeae8da85787b25313616
Thomas Asamba <asamba.thomas@gmail.com>
2025-12-30 10:15:33 +0300

    fix(security): suppress bandit false positives and exclude tests

---
+++ README.md
@@ -0,0 +1,1 @@
+# Triggering CI/CD - Tue Dec 30 09:47:53 EAT 2025
--- app/src/routes/main.py
+++ app/src/routes/main.py
@@ -75,4 +75,4 @@ def metrics():
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8000) # nosec

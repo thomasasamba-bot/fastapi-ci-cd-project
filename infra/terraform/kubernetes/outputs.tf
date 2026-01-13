1daf06e83a4f8075e39baa7ab72c97533959007f
Thomas Asamba <asamba.thomas@gmail.com>
2026-01-13 12:31:40 +0300

    fix(terraform): update outputs to use renamed cluster_node resource

--- infra/terraform/kubernetes/outputs.tf
+++ infra/terraform/kubernetes/outputs.tf
@@ -1,11 +1,11 @@
-output "k3s_node_public_ip" {
-  value = aws_instance.k3s_node.public_ip
+output "cluster_node_public_ip" {
+  value = aws_instance.cluster_node.public_ip
 }
 
-output "k3s_node_public_dns" {
-  value = aws_instance.k3s_node.public_dns
+output "cluster_node_public_dns" {
+  value = aws_instance.cluster_node.public_dns
 }
 
 output "ssh_command" {
-  value = "ssh ubuntu@${aws_instance.k3s_node.public_ip}" # Requires key pair if assigned, using SSM is better
+  value = "ssh ubuntu@${aws_instance.cluster_node.public_ip}" # Requires key pair if assigned, using SSM is better
 }
